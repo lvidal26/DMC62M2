59859f5b4d8899b38fff329ffe178cf42fdc78a3
lvidal26 <luisvidal12336@gmail.com>
2026-09-17 22:00:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+st.title("Manejo de Dataframes")
+st.sidebar.title("Herramientas")
+
+archivo = st.sidebar.file_uploader("Selecciona tu archivo a cargar")
+if archivo is not None:
+  st.write("Su archivo ha sido cargado exitosamente")
+
+  if archivo.name.endswith(".csv"):
+    datos = pd.read_csv(archivo)
+  if archivo.name.endswith(".xlsx"):
+    datos = pd.read_excel(archivo)
+  st.write(datos)
+else:
+  st.write("Cargue su archivo")
